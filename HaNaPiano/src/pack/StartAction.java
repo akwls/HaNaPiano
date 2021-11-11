f6286c6e5877d9c4f99371f2dbcddcdde1a4880c
Kim Yu Na <yunasun0920@gmail.com>
2021-11-11 14:46:58 +0900

    버튼 생성

--- HaNaPiano/src/pack/StartAction.java
+++ HaNaPiano/src/pack/StartAction.java
@@ -1,15 +1,22 @@
 package pack;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonModel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class StartAction extends JFrame implements ActionListener {
 	public JFrame frame;
@@ -22,7 +29,7 @@ public class StartAction extends JFrame implements ActionListener {
 		setResizable(false);//창의 크기를 변경하지 못하게
 		setLocationRelativeTo(null);//창이 가운데 나오게
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//JFrame이 정상적으로 종료되게
-		
+		Start_Screen();
 	}
 	
 	@Override
@@ -30,19 +37,166 @@ public class StartAction extends JFrame implements ActionListener {
 		// TODO Auto-generated method stub4
 		frame.setVisible(false);
 		setVisible(true);
-		
 	}
 
-	@Override
-	public void paint(Graphics g) {
-		// TODO Auto-generated method stub
-		super.paint(g);
-		g.setColor(new Color(255, 231, 110));
-		g.fillRect(0, 0, 1600, 180);
-		ImageIcon icon = new ImageIcon("../HaNaPiano/src/image/piano_background.png");
-        Image img = icon.getImage();
-		g.drawImage(img, 0, 180, 1600, 800, null);
+	public void Start_Screen() {
+		ImageIcon image = new ImageIcon("../HaNaPiano/src/image/play_start.png");
+		JLabel jl = new JLabel(image);
+		jl.setSize(1600, 900);
+		this.add(jl);
+		this.setLayout(null);
+		this.setVisible(true);
+		
+		JButton btnMusic = new JButton();
+		JButton btnUp = new JButton();
+		JButton btnDawn = new JButton();
+		JButton btnRecoding = new JButton();
+		JButton btnStop = new JButton();
+		JButton btnPause = new JButton();
+		JButton btnPlay = new JButton();
+		
+		// 악보보기
+		btnMusic.setBounds(20, 40, 100, 110);
+		btnMusic.setOpaque(false);
+		btnMusic.setContentAreaFilled(false);
+		btnMusic.setBorderPainted(true);
+		btnMusic.setFocusable(false);
+		
+		btnMusic.getModel().addChangeListener(new ChangeListener() {
+	        @Override
+	        public void stateChanged(ChangeEvent e) {
+	            ButtonModel model = (ButtonModel) e.getSource();
+	            if (model.isRollover()) {
+	            	btnMusic.setBorderPainted(true); // 테두리 보이게
+	            } else {
+	            	btnMusic.setBorderPainted(false); // 테두리 안보이게
+	            }
+	        }
+	    });
+		
+		jl.add(btnMusic);
+		
+		// 옥타브 올리기
+		btnUp.setBounds(635, 70, 50, 50);
+		btnUp.setOpaque(false);
+		btnUp.setContentAreaFilled(false);
+		btnUp.setBorderPainted(true);
+		btnUp.setFocusable(false);
+		
+		btnUp.getModel().addChangeListener(new ChangeListener() {
+	        @Override
+	        public void stateChanged(ChangeEvent e) {
+	            ButtonModel model = (ButtonModel) e.getSource();
+	            if (model.isRollover()) {
+	            	btnUp.setBorderPainted(true); // 테두리 보이게
+	            } else {
+	            	btnUp.setBorderPainted(false); // 테두리 안보이게
+	            }
+	        }
+	    });
+		
+		jl.add(btnUp);
+		
+		// 옥타브 내리기
+		btnDawn.setBounds(918, 70, 50, 50);
+		btnDawn.setOpaque(false);
+		btnDawn.setContentAreaFilled(false);
+		btnDawn.setBorderPainted(true);
+		btnDawn.setFocusable(false);
+
+		btnDawn.getModel().addChangeListener(new ChangeListener() {
+	        @Override
+	        public void stateChanged(ChangeEvent e) {
+	            ButtonModel model = (ButtonModel) e.getSource();
+	            if (model.isRollover()) {
+	            	btnDawn.setBorderPainted(true); // 테두리 보이게
+	            } else {
+	            	btnDawn.setBorderPainted(false); // 테두리 안보이게
+	            }
+	        }
+	    });
+		jl.add(btnDawn);
+		
+		// 녹음하기
+		btnRecoding.setBounds(1103, 55, 70, 70);
+		btnRecoding.setOpaque(false);
+		btnRecoding.setContentAreaFilled(false);
+		btnRecoding.setBorderPainted(true);
+		btnRecoding.setFocusable(false);
+
+		btnRecoding.getModel().addChangeListener(new ChangeListener() {
+	        @Override
+	        public void stateChanged(ChangeEvent e) {
+	            ButtonModel model = (ButtonModel) e.getSource();
+	            if (model.isRollover()) {
+	            	btnRecoding.setBorderPainted(true); // 테두리 보이게
+	            } else {
+	            	btnRecoding.setBorderPainted(false); // 테두리 안보이게
+	            }
+	        }
+	    });
+		jl.add(btnRecoding);
+		
+		// 녹음 멈추기
+		btnStop.setBounds(1185, 55, 70, 70);
+		btnStop.setOpaque(false);
+		btnStop.setContentAreaFilled(false);
+		btnStop.setBorderPainted(true);
+		btnStop.setFocusable(false);
+		
+		btnStop.getModel().addChangeListener(new ChangeListener() {
+	        @Override
+	        public void stateChanged(ChangeEvent e) {
+	            ButtonModel model = (ButtonModel) e.getSource();
+	            if (model.isRollover()) {
+	            	btnStop.setBorderPainted(true); // 테두리 보이게
+	            } else {
+	            	btnStop.setBorderPainted(false); // 테두리 안보이게
+	            }
+	        }
+	    });
+		jl.add(btnStop);
+		
+		// 음악 재생
+		btnPlay.setBounds(1263, 55, 70, 70);
+		btnPlay.setOpaque(false);
+		btnPlay.setContentAreaFilled(false);
+		btnPlay.setBorderPainted(true);
+		btnPlay.setFocusable(false);
+		
+		btnPlay.getModel().addChangeListener(new ChangeListener() {
+	        @Override
+	        public void stateChanged(ChangeEvent e) {
+	            ButtonModel model = (ButtonModel) e.getSource();
+	            if (model.isRollover()) {
+	            	btnPlay.setBorderPainted(true); // 테두리 보이게
+	            } else {
+	            	btnPlay.setBorderPainted(false); // 테두리 안보이게
+	            }
+	        }
+	    });
+		jl.add(btnPlay);
+		
+		// 음악 정지
+		btnPause.setBounds(1345, 55, 70, 70);
+		btnPause.setOpaque(false);
+		btnPause.setContentAreaFilled(false);
+		btnPause.setBorderPainted(true);
+		btnPause.setFocusable(false);
+		
+		btnPause.getModel().addChangeListener(new ChangeListener() {
+	        @Override
+	        public void stateChanged(ChangeEvent e) {
+	            ButtonModel model = (ButtonModel) e.getSource();
+	            if (model.isRollover()) {
+	            	btnPause.setBorderPainted(true); // 테두리 보이게
+	            } else {
+	            	btnPause.setBorderPainted(false); // 테두리 안보이게
+	            }
+	        }
+	    });
+		jl.add(btnPause);
+		
 	}
-	
 
 }
